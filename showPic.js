35140b26385451f1f798695a62ec7751270c00e4
Sharon <429039294@qq.com>
2019-04-09 23:56:28 +0800

    动态创建标记

--- showPic.js
+++ showPic.js
@@ -51,6 +51,45 @@ function prepareGallery(){
 	}
 }
 
+function insertAfter(newElement,targetElement){
+	var parent = targetElement.parentNode;
+	if(parent.lastChild == targetElement){
+		parent.appendChild(newElement);
+	}else{
+		parent.insertBefore(newElement,targetElement.nextSibling);
+	}
+}
+
+function preparePlaceholder(){
+	if(!document.createElement){
+		return false;
+	}
+	if(!document.createTextNode){
+		return false;
+	}
+	if(!document.getElementById){
+		return false;
+	}
+	if(!document.getElementById("imagegallery")){
+		return false;
+	}
+
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","images/zhanwei.jpg");
+	placeholder.setAttribute("alt","my image gallery");
+
+	var description = document.createElement("p");
+	description.setAttribute("id","description");
+
+	var desctext = document.createTextNode("Choose an image");
+	description.appendChild(desctext);
+
+	var gallery = document.getElementById("imagegallery");
+	insertAfter(placeholder,gallery);
+	insertAfter(description,placeholder);
+}
+
 //共享onload事件
 function addLoadEvent(func){
 	var oldonload = window.onload;
@@ -64,4 +103,5 @@ function addLoadEvent(func){
 	}
 }
 
+addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
