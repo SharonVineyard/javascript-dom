76b85fcca8f788f9f91de9fa29bcf0495f3785b7
Sharon <429039294@qq.com>
2019-04-13 18:00:24 +0800

    add displayCitations

---
+++ scripts/displayCitations.js
@@ -0,0 +1,29 @@
+function displayCitations(){
+	if(!document.getElementsByTagName) return false;
+	if(!document.createTextNode) return false;
+	if(!document.createElement) return false;
+
+	var quotes = document.getElementsByTagName("blockquote");
+	for(var i = 0;i < quotes.length;i++){
+		if(!quotes[i].getAttribute("cite")){
+			continue;
+		}
+		var url = quotes[i].getAttribute("cite");
+		var quoteChildren = quotes[i].getElementsByTagName("*");
+		if(quoteChildren.length < 1) continue;
+		var elem = quoteChildren[quoteChildren.length - 1];
+
+		var link = document.createElement("a");
+		var link_text = document.createTextNode("source");
+		link.appendChild(link_text);
+		link.setAttribute("href",url);alert("3");
+
+		//sup元素能使元素在浏览器上呈现上标的效果
+		var superscript = document.createElement("sup");
+		superscript.appendChild(link);
+
+		elem.appendChild(superscript);
+	}
+}
+
+addLoadEvent(displayCitations);
